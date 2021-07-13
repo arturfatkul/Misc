e60c044c4e7279b70915fa277622e66c6a0d867b
Artur Fatkulin <fatkulinartur@gmail.com>
2021-07-13 21:13:50 +0100

    collatz

---
+++ collatz.py
@@ -0,0 +1,15 @@
+def collatz(number):
+    
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+
+number_input = input("Enter the number: ")
+
+while number_input != 1:
+    number_input = collatz(int(number_input))
+
